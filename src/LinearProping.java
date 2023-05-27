07be4185edccbe4ff1387490018fb686a57fb3cc
GIL <gil.00000032610@unicap.br>
2023-05-27 11:20:02 -0300

    first commit

---
+++ src/LinearProping.java
@@ -0,0 +1,11 @@
+public class LinearProping {
+   private String[] hashTable;
+    private int usedCells;
+
+    public LinearProping(int size){
+        hashTable = new String[size];
+        usedCells = 0;
+    }
+
+
+}
